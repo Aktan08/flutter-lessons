f21c5b984ec1a8e2743fde2bdde237e0d2a6b277
Aktan08 <40670907+Aktan08@users.noreply.github.com>
2022-12-06 14:59:27 +0600

    Create dart_lesson.dart

---
+++ lessons/dart_lesson/bin/dart_lesson.dart
@@ -0,0 +1,8 @@
+import 'package:dart_lesson/dart_lesson.dart' as dart_lesson;
+
+void main(List<String> arguments) {
+  List cars = ['m','b','v','g'];
+
+  Map kfc = {'rols' : 200, 'basket': 400, };
+
+}
